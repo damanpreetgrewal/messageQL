df9474929a48ba1215b82cd49302fad1d4560c0a
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2022-11-14 21:34:01 -0500

    Chakra Theme

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,13 +3,12 @@ import type { NextPage, NextPageContext } from 'next';
 import { getSession, useSession } from 'next-auth/react';
 import Auth from '../components/Auth/Auth';
 import Chat from '../components/Chat/Chat';
+import { Session } from 'next-auth';
 
 const Home: NextPage = () => {
   const { data } = useSession();
 
-  return
-  <Box>{data?.user ? <Chat /> : <Auth />}</Box>;
-
+  return <Box>{data?.user?.username ? <Chat /> : <Auth />}</Box>;
 };
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -17,8 +16,8 @@ export async function getServerSideProps(context: NextPageContext) {
   return {
     props: {
       session,
-    }
-  }
+    },
+  };
 }
 
 export default Home;
