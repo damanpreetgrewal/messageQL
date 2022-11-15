658db49f662c9437216b611a431ea38e1dbcdabf
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2022-11-15 13:23:32 -0500

    Setup Backend - install dependencies

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -1,13 +1,14 @@
-import { Button } from '@chakra-ui/react';
 import { signOut } from 'next-auth/react';
 
-interface IChatProps {
-}
+interface IChatProps {}
 
-const Chat: React.FC<IChatProps> = (props) => {
-    return <div>CHAT
-        <button onClick={() => signOut()}>Logout</button>
-    </div>;
+const Chat: React.FC<IChatProps> = props => {
+  return (
+    <div>
+      CHAT
+      <button onClick={() => signOut()}>Logout</button>
+    </div>
+  );
 };
 
 export default Chat;
